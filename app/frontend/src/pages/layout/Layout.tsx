65cdb8d202d9039169831b5659dbf9e1698d3c5a
abhiaryz <abhishekaryz@gmail.com>
2024-06-05 14:46:00 +0530

    title icon changed

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -29,7 +29,7 @@ const Layout = () => {
                                 </NavLink>
                             </li>
                             <li className={styles.headerNavLeftMargin}>
-                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
+                                <a href="https://www.thebrights.ai/" target={"_blank"} title="Github repository link">
                                     <img
                                         src={github}
                                         alt="Github logo"
